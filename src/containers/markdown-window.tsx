88320488fe7e1be83d9308aafbce7c25342b05b0
axseinga <urbanowicz.uk@gmail.com>
2025-01-25 13:33:23 +0000

    add border to markdown preview

--- src/containers/markdown-window.tsx
+++ src/containers/markdown-window.tsx
@@ -10,7 +10,7 @@ export const MarkdownWindow = ({
   handleChange,
 }: MarkdownWindowProps) => {
   return (
-    <div>
+    <div className="border-r-2 border-customGrey-300">
       <WindowHeading title="Markdown" hideToggleButtonOnDesktop />
       <textarea
         className="markdown h-full w-full whitespace-pre-line bg-transparent p-5 focus:outline-none"
